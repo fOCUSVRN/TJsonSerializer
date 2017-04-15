222ce513c21d8da170b969080812fbb88a7eadf0
fOCUSVRN <v-focus@mail.ru>
2017-04-15 10:42:22 +0300

    add classcloner, TxmlDeserializer

--- uJsonSerializer.pas
+++ uJsonSerializer.pas
@@ -4,6 +4,11 @@ interface
 
 uses sysutils, classes, generics.collections;
 
+// чтобы работать с атрибутами, нужно в модуле, где описывается модель добавить в uses superobject
+
+// юзать так:
+// [SOName('jsonfieldname')]]
+
 type
   TJsonSerializer = class
   public
@@ -11,9 +16,19 @@ type
     class function Deserialize<T>(const Json: string): T;
   end;
 
+  TClassCloner = class
+    class function Clone(aValue: TObject): TObject; overload;
+    class function Clone<T: class>(aValue: TObject): T; overload;
+  end;
+
+  TXMLSerializer = class
+  public
+    class function Deserialize<T>(const XML: string): T;
+  end;
+
 implementation
 
-uses superobject;
+uses superobject, dbxjsonreflect, Json, rtti, typinfo, controls, superxmlparser;
 
 { TJsonSerializer }
 
@@ -36,6 +51,50 @@ begin
   ctx := TSuperRttiContext.Create;
   try
     result := ctx.AsJson(obj).AsJson(true, false);
+  finally
+    freeandnil(ctx);
+  end;
+end;
+
+{ TClassCloner }
+
+class function TClassCloner.Clone(aValue: TObject): TObject;
+var
+  MarshalObj: TJSONMarshal;
+  UnMarshalObj: TJSONUnMarshal;
+  JSONValue: TJSONValue;
+begin
+  result := nil;
+  MarshalObj := TJSONMarshal.Create;
+  UnMarshalObj := TJSONUnMarshal.Create;
+  try
+    JSONValue := MarshalObj.Marshal(aValue);
+    try
+      if Assigned(JSONValue) then
+        result := UnMarshalObj.Unmarshal(JSONValue);
+    finally
+      JSONValue.Free;
+    end;
+  finally
+    MarshalObj.Free;
+    UnMarshalObj.Free;
+  end;
+end;
+
+class function TClassCloner.Clone<T>(aValue: TObject): T;
+begin
+  result := Clone(aValue) as T;
+end;
+
+{ TXMlDeserializer }
+
+class function TXMLSerializer.Deserialize<T>(const XML: string): T;
+var
+  ctx: TSuperRttiContext;
+begin
+  ctx := TSuperRttiContext.Create;
+  try
+    result := ctx.AsType<T>(XMLParseString(XML, true));
   finally
     freeandnil(ctx);
   end;
